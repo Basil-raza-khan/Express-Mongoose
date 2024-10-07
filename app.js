18dfa34dada714102953d908063172bd2239d11f
Your Name <15062@kiet.edu.pk>
2024-10-07 21:34:39 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,53 @@
+const express = require('express');
+const path = require('path');
+const mongoose = require('mongoose');
+const userModel = require('./models/usermodel');
+require('dotenv').config();
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Connect to MongoDB
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+  .then(() => console.log('Connected to MongoDB'))
+  .catch(err => console.log('Failed to connect to MongoDB', err));
+
+app.set('view engine', 'ejs');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get('/', (req, res) => {
+    res.render('index');
+});
+
+app.post('/create', async (req, res) => {
+    let { name, email, image } = req.body;
+    let user = await userModel.create({ name, email, image });
+    res.redirect('read');
+});
+
+app.get('/read', async (req, res) => {
+    let users = await userModel.find();
+    res.render('read', { users });
+});
+
+app.get('/delete/:id', async (req, res) => {
+    await userModel.findOneAndDelete({ _id: req.params.id });
+    res.redirect('/read');
+});
+
+app.get('/edit/:userid', async (req, res) => {
+    let user = await userModel.findOne({ _id: req.params.userid });
+    res.render('edit', { user });
+});
+
+app.post('/update/:userid', async (req, res) => {
+    let { name, email, image } = req.body;
+    await userModel.findOneAndUpdate({ _id: req.params.userid }, { name, email, image }, { new: true });
+    res.redirect('/read');
+});
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
---
+++ models/usermodel.js
@@ -0,0 +1,12 @@
+// models/usermodel.js
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose.Schema({
+    name: String,
+    email: String,
+    image: String
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
